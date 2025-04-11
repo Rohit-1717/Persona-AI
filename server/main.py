138e56198ef7a05bda7c59c8ec87632e6ba33f8c
ROHIT VISHWAKARMA <111670340+Rohit-1717@users.noreply.github.com>
2025-04-11 15:56:38 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://persona-ai-dxg8.onrender.com"],  # Adjust this for your frontend origin
+    allow_origins=["http://localhost:5173","https://persona-ai-git-main-rohit-vishwakarmas-projects-95176ae8.vercel.app"],  # Adjust this for your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
